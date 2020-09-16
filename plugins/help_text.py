7fe14e1d5ebd4af9f4fcb8642e0a2029f08d060c
rohith7187 <68913285+rohith7187@users.noreply.github.com>
2020-09-16 13:53:38 +0000

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -37,6 +37,15 @@ def GetExpiryDate(chat_id):
     Config.AUTH_USERS.add(861055237)
     return expires_at
 
+return expires_at  
+Owner_id = [555549602, 700923542]
+Member = [555549602, 700923542]  
+@pyrogram.Client.on_message()
+async def star(bot, update):       if update.from_user.id not in Member:           await bot.send_message(chat_id=update.chat.id, text="Hi {} I am a renamer bot for specially [rohith](https://t.me/roHiTh_rio).Ask him to use me.".format(update.from_user.first_name),                               reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('My Father', url='https://t.me/roHiTh_rio')]]))        if update.from_user.id in Member:        update.continue_propagation() 
+@pyrogram.Client.on_message(pyrogram.Filters.command(["rio"]))
+async def rio(bot, update):   if len(update.command) == 2:    add = int(update.text.split(' ')[1])    if update.from_user.id in Owner_id:     if add not in Member:      await bot.send_message(         chat_id=update.chat.id,         text="successfully added {}. To use me".format(add),         reply_to_message_id=update.message_id         )      return Member.append(add)     if add in Member:       await update.reply_text(text='Hello Boss he is already in the members who are allowed to use me')    if update.from_user.id not in Owner_id:      await bot.send_message(         chat_id=update.chat.id,         text="your are a member of this bot not the owner so you can't add any one.",         reply_to_message_id=update.message_id         )   if len(update.command) == 1:           await bot.send_message(         chat_id=update.chat.id,         text="No id found.",         reply_to_message_id=update.message_id         ) 
+@pyrogram.Client.on_message(pyrogram.Filters.command(["paytm"]))
+async def paytm(bot, update):   if len(update.command) == 2:    unadd = int(update.text.split(' ')[1])    if update.from_user.id in Owner_id:      if unadd in Member:         await bot.send_message(            chat_id=update.chat.id,            text="successfully removed id {} from use me".format(unadd),            reply_to_message_id=update.message_id            )         return Member.remove(unadd)      if unadd not in Member:         await bot.send_message(            chat_id=update.chat.id,            text="id {} not the member to use this bot.Please try the id of person who is the member for removing".format(unadd),            reply_to_message_id=update.message_id            )    if update.from_user.id not in Owner_id:      await bot.send_message(         chat_id=update.chat.id,         text="your are a member of this bot not the owner so you can't remove any one.",         reply_to_message_id=update.message_id         )   if len(update.command) == 1:           await bot.send_message(         chat_id=update.chat.id,         text="No id found.",         reply_to_message_id=update.message_id         )
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["help"]))
 async def help_user(bot, update):
